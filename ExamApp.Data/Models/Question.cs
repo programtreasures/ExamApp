86160fd5e2d3105befa8e3d82c743db03400ee56
techtrainer55 <technical.trainer7@gmail.com>
2017-12-28 09:47:00 +0530

    Add project files.

---
+++ ExamApp.Data/Models/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamApp.Data.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        public string QuestionSubject { get; set; }
+
+        public string QuestionBody { get; set; }
+    }
+}
